55e03f8336a33db7949a2d28b299e0e09cf723fa
dvo1 <duosinmivictory1@gmail.com>
2023-10-12 08:00:10 +0100

    feat: second commit

--- src/components/features/projects.tsx
+++ src/components/features/projects.tsx
@@ -1,7 +1,12 @@
-import React, { useEffect, useRef }  from 'react'
+import React, { useEffect, useRef } from 'react'
 import { Box, Button, Typography, useMediaQuery } from '@mui/material'
 import { motion, useInView, useAnimation } from 'framer-motion'
-
+import Link from 'next/link';
+import Image from "next/image";
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import dashboard from '../../../public/assets/dashboard.png'
 const Projects = () => {
     const ref = useRef(null)
     const isInView = useInView(ref, { once: true })
@@ -22,16 +27,35 @@ const Projects = () => {
             y: 0,
         }
     }
-  return (
-    <Box  sx={{
-        width: { xs: '800%', md: '80%' },
-        margin: 'auto',
-    }} ref={ref}>
-        <motion.div>
+    return (
+        <Box sx={{
+            width: { xs: '100%', md: '80%' },
+            margin: 'auto',
+        }} ref={ref}>
+            <motion.div>
+                <Box sx={{
+                    width: { xs: '80%', md: '50%' },
+                    margin: 'auto',
+                }}>
+                    <Card>
+                        <Image
+                            src={dashboard}
+                            alt='dashboard'
+                            style={{
+                                width: '248px',
+                                height: '240px',
+                                objectFit: 'cover'
+                            }}
+                        />
+                        <CardContent>
+                            <Box>dajfiol</Box>
+                        </CardContent>
+                    </Card>
+                </Box>
 
-        </motion.div>
-    </Box>
-  )
+            </motion.div>
+        </Box>
+    )
 }
 
 export default Projects
